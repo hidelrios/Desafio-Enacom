544b236318778b1f819ed82d9f38b8ad8008be71
H1d3l <hidels.rios@gmail.com>
2023-07-17 22:34:46 -0300

    aplicado correcao no codigo

--- main.py
+++ main.py
@@ -1,57 +1,57 @@
-from pulp import *
+from pulp import LpMaximize, LpProblem, LpStatus, LpVariable
 import pandas as pd
 
 
-def constraint_total_budget(projects: list, decision_variable: dict, expected_cost_info: dict):
-    # Restrição: Orçamento total
-    return lpSum(expected_cost_info[project] * decision_variable[project] for project in projects) <= 2400000
+def constraint_total_budget(projects: list, decision_variable: list, expected_cost_info: list, total_budget: int):
+    return sum(expected_cost_info[i] * decision_variable[i] for i in range(len(projects))) <= total_budget
 
 
-def constraint_at_least_one_high_risk_investment(projects: list, decision_variable: dict, investment_risk: dict):
-    # Restrição: Pelo menos 1 investimento de risco alto
-    return lpSum(decision_variable[project] for project in projects if investment_risk[project] == 'Alto') >= 1
+def constraint_at_least_one_high_risk_investment(projects: list, decision_variable: list, investment_risk: list):
+    return sum(decision_variable[i] for i in range(len(projects)) if investment_risk[i] == 'Alto') >= 1
 
 
-def constraint_at_least_two_medium_risk_investment(projects: list, decision_variable: dict, investment_risk: dict):
-    # Restrição: Pelo menos 2 investimentos de risco médio
-    return lpSum(decision_variable[project] for project in projects if investment_risk[project] == 'Médio') >= 2
+def constraint_at_least_two_medium_risk_investment(projects: list, decision_variable: list, investment_risk: list):
+    return sum(decision_variable[i] for i in range(len(projects)) if investment_risk[i] == 'Médio') >= 2
 
 
-def constraint_at_least_two_low_risk_investment(projects: list, decision_variable: dict, investment_risk: dict):
-    # Restrição: Pelo menos 2 investimentos de risco baixo
-    return lpSum(decision_variable[project] for project in projects if investment_risk[project] == 'Baixo') >= 2
+def constraint_at_least_two_low_risk_investment(projects: list, decision_variable: list, investment_risk: list):
+    return sum(decision_variable[i] for i in range(len(projects)) if investment_risk[i] == 'Baixo') >= 2
 
 
-def constraint_maximum_cost_for_low_risk_investments(projects: list, decision_variable: dict, expected_cost_info: dict, investment_risk: dict):
-    # Restrição: Custo máximo para investimentos de risco baixo
-    return lpSum(expected_cost_info[project] * decision_variable[project] for project in projects if investment_risk[project] == 'Baixo') <= 1200000
+def constraint_maximum_cost_for_low_risk_investments(projects: list, decision_variable: list, expected_cost_info: list, investment_risk: list, low_maximum_cost: int):
+    return sum(expected_cost_info[i] * decision_variable[i] for i in range(len(projects)) if investment_risk[i] == 'Baixo') <= low_maximum_cost
 
 
-def constraint_maximum_cost_for_medium_risk_investments(projects: list, decision_variable: dict, expected_cost_info: dict, investment_risk: dict):
-    # Restrição: Custo máximo para investimentos de risco baixo
-    return lpSum(expected_cost_info[project] * decision_variable[project] for project in projects if investment_risk[project] == 'Médio') <= 150000
+def constraint_maximum_cost_for_medium_risk_investments(projects: list, decision_variable: list, expected_cost_info: list, investment_risk: list, average_maximum_cost: int):
+    return sum(expected_cost_info[i] * decision_variable[i] for i in range(len(projects)) if investment_risk[i] == 'Médio') <= average_maximum_cost
 
 
-def constraint_maximum_cost_for_high_risk_investments(projects: list, decision_variable: dict, expected_cost_info: dict, investment_risk: dict):
-    # Restrição: Custo máximo para investimentos de risco baixo
-    return lpSum(expected_cost_info[project] * decision_variable[project] for project in projects if investment_risk[project] == 'Alto') <= 900000
+def constraint_maximum_cost_for_high_risk_investments(projects: list, decision_variable: list, expected_cost_info: list, investment_risk: list, cost_maximum_high: int):
+    return sum(expected_cost_info[i] * decision_variable[i] for i in range(len(projects)) if investment_risk[i] == 'Alto') <= cost_maximum_high
 
 
 def main():
 
-    # Adicionando arquivo com informações
+    # Carrega informações
     df = pd.read_csv(r'D:\Projetos\Desafio Enacom\teste.csv', sep=';')
 
     # Retorno esperado de cada projeto
-    expect_return_info = {}
+    expect_return_info = []
 
     # Custo de cada projeto
-    expected_cost_info = {}
+    expected_cost_info = []
 
     # Risco do investimento
-    investment_risk = {}
+    investment_risk = []
 
+    # Projetos
     projects = []
+
+    # Dados de exemplo
+    total_budget = 2400000
+    low_maximum_cost = 1200000
+    average_maximum_cost = 1500000
+    cost_maximum_high = 900000
 
     # Tratamento das informações do arquivo
     for index, line in df.iterrows():
@@ -61,68 +61,72 @@ def main():
         risk = line['Risco do investimento']
 
         projects.append(project)
+        expect_return_info.append(expect_return)
+        expected_cost_info.append(expect_cost)
+        investment_risk.append(risk)
 
-        expect_return_info[project] = expect_return
-        expected_cost_info[project] = expect_cost
-        investment_risk[project] = risk
+    # Cria o problema de maximização
+    problem = LpProblem("Otimização de Projetos", LpMaximize)
 
-    # Inicializa uma declaração do problema
-    prob = LpProblem(name="Maximizar retorno", sense=LpMaximize)
+    # Definir variáveis de decisão
+    xi = []
+    for i in range(len(projects)):
+        xi.append(LpVariable(f"x_{i}", lowBound=0, upBound=1, cat='Binary'))
 
-    # Define as variáveis de decisão
-    decision_variable = LpVariable.dicts(
-        name="Selecionado", indices=projects, lowBound=0, upBound=1, cat=LpBinary)
+    # Definir função objetivo
+    objective_function = sum(
+        expect_return_info[i] * xi[i] for i in range(len(projects)))
+    problem.setObjective(objective_function)
 
-    # Definição da função objetiva maximizar Z = Σ (retorno_esperado * variavel de decisão) para todos os projetos project
-    prob += lpSum(expect_return_info[project] *
-                  decision_variable[project] for project in projects)
+    # Definir restrições
+    # Restrição de orçamento total
+    total_budget_restriction = constraint_total_budget(
+        projects=projects, decision_variable=xi, expected_cost_info=expected_cost_info, total_budget=total_budget)
+    problem.addConstraint(total_budget_restriction)
 
-    # Restrição: Orçamento total
-    prob += constraint_total_budget(projects=projects,
-                                    decision_variable=decision_variable, expected_cost_info=expected_cost_info)
+    # Restrição de pelo menos 1 investimento de risco alto
+    high_risk_constrain = constraint_at_least_one_high_risk_investment(
+        projects=projects, decision_variable=xi, investment_risk=investment_risk)
+    problem.addConstraint(high_risk_constrain)
 
-    # Restrição: Pelo menos 1 investimento de risco alto
-    prob += constraint_at_least_one_high_risk_investment(
-        projects=projects, decision_variable=decision_variable, investment_risk=investment_risk)
+    # Restrição de pelo menos 2 investimentos de risco médio
+    medium_risk_constrain = constraint_at_least_two_medium_risk_investment(
+        projects=projects, decision_variable=xi, investment_risk=investment_risk)
+    problem.addConstraint(medium_risk_constrain)
 
-    # Restrição: Pelo menos 2 investimentos de risco médio
-    prob += constraint_at_least_two_medium_risk_investment(
-        projects=projects, decision_variable=decision_variable, investment_risk=investment_risk)
+    # Restrição de pelo menos 2 investimentos de risco baixo
+    low_risk_constrain = constraint_at_least_two_low_risk_investment(
+        projects=projects, decision_variable=xi, investment_risk=investment_risk)
+    problem.addConstraint(low_risk_constrain)
 
-    # Restrição: Pelo menos 2 investimentos de risco baixo
-    prob += constraint_at_least_two_low_risk_investment(
-        projects=projects, decision_variable=decision_variable, investment_risk=investment_risk)
+    # Restrição de custo máximo para investimentos de risco baixo
+    constraint_cost_low = constraint_maximum_cost_for_low_risk_investments(
+        projects=projects, decision_variable=xi, investment_risk=investment_risk, expected_cost_info=expected_cost_info, low_maximum_cost=low_maximum_cost)
+    problem.addConstraint(constraint_cost_low)
 
-    # Restrição: Custo máximo para investimentos de risco baixo
-    prob += constraint_maximum_cost_for_low_risk_investments(
-        projects=projects, decision_variable=decision_variable, expected_cost_info=expected_cost_info, investment_risk=investment_risk)
+    # Restrição de custo máximo para investimentos de risco médio
+    constraint_cost_medium = constraint_maximum_cost_for_medium_risk_investments(
+        projects=projects, decision_variable=xi, investment_risk=investment_risk, expected_cost_info=expected_cost_info, average_maximum_cost=average_maximum_cost)
+    problem.addConstraint(constraint_cost_medium)
 
-    # Restrição: Custo máximo para investimentos de risco médio
-    prob += constraint_maximum_cost_for_medium_risk_investments(
-        projects=projects, decision_variable=decision_variable, expected_cost_info=expected_cost_info, investment_risk=investment_risk)
-
-    # Restrição: Custo máximo para investimentos de risco alto
-    prob += constraint_maximum_cost_for_high_risk_investments(
-        projects=projects, decision_variable=decision_variable, expected_cost_info=expected_cost_info, investment_risk=investment_risk)
+    # Restrição de custo máximo para investimentos de risco alto
+    constraint_cost_high = constraint_maximum_cost_for_high_risk_investments(
+        projects=projects, decision_variable=xi, investment_risk=investment_risk, expected_cost_info=expected_cost_info, cost_maximum_high=cost_maximum_high)
+    problem.addConstraint(constraint_cost_high)
 
     # Resolver o problema
-    status = prob.solve()
+    status = problem.solve()
 
     if status == 1:  # Problema resolvido com sucesso
         # Imprimir a solução
         print("Solução encontrada:")
-
-        # Imprimindo a solução
-        print("Status:", LpStatus[prob.status])
-        print("Retorno Total Esperado: R$", value(prob.objective))
-
-        for project in projects:
-            if decision_variable[project].value() == 1:
+        for i, value in enumerate(projects):
+            if xi[i].value() == 1:
                 print(
-                    f"Projeto {project}: Retorno Esperado = {expect_return_info[project]}, Custo = {expected_cost_info[project]}, Risco = {investment_risk[project]}")
+                    f"Projeto {i} - {value}: Retorno Esperado = {expect_return_info[i]}, Custo = {expected_cost_info[i]}, Risco = {investment_risk[i]}")
     else:
         print("Não foi encontrada uma solução ótima.")
 
 
-if __name__:
+if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -5,35 +5,33 @@ from pulp import *
 
 class TestTotalBudgetConstraint(unittest.TestCase):
 
-    def set_up(self):
-        self.projetos = ['projeto1','projeto2','projeto3','projeto4']
-        self.retornos = {'projeto1': 100000, 'projeto2': 150000, 'projeto3': 200000, 'projeto4': 120000}
-        self.custos = {'projeto1': 800000, 'projeto2': 500000, 'projeto3': 700000, 'projeto4': 400000}
-        self.riscos = {'projeto1': 'Baixo', 'projeto2': 'Médio', 'projeto3': 'Alto', 'projeto4': 'Médio'}
-        self.limite_custos = {'Baixo': 1200000, 'Médio': 1500000, 'Alto': 900000}
-        self.decision_variable = LpVariable.dicts(name="Selecionado", indices=self.projetos, lowBound=0, upBound=1, cat=LpBinary)
+    def setUp(self):
+        self.projects = ['projeto1','projeto2','projeto3','projeto4']
+        self.expect_return = [100000, 150000,  200000,  120000]
+        self.expected_cost = [800000, 500000, 700000, 400000]
+        self.investment_risk = ['Baixo',  'Médio',  'Alto', 'Médio']
+        self.decision_variable = [LpVariable(f"x_{i}", lowBound=0, upBound=1, cat='Binary') for i in range(len(self.projects))]
 
     def test_total_budget_constraint(self):
-        self.assertTrue(constraint_total_budget(self.projetos, self.decision_variable, self.retornos))
+        self.assertTrue(constraint_total_budget(projects=self.projects,decision_variable= self.decision_variable, total_budget=2400000, expected_cost_info=self.expected_cost))
 
     def test_constraint_at_least_one_high_risk_investment(self):
-        self.assertTrue(constraint_at_least_one_high_risk_investment(self.projetos, self.decision_variable, self.riscos))
+        self.assertTrue(constraint_at_least_one_high_risk_investment(self.projects,decision_variable= self.decision_variable,investment_risk= self.investment_risk))
 
     def test_constraint_at_least_two_medium_risk_investment(self):
-        self.assertTrue(constraint_at_least_two_medium_risk_investment(self.projetos, self.decision_variable, self.riscos))
+        self.assertTrue(constraint_at_least_two_medium_risk_investment(projects=self.projects, decision_variable=self.decision_variable, investment_risk=self.investment_risk))
 
     def test_constraint_at_least_two_low_risk_investment(self):
-        self.assertTrue(constraint_at_least_two_low_risk_investment(self.projetos, self.decision_variable, self.riscos))
+        self.assertTrue(constraint_at_least_two_low_risk_investment(projects=self.projects,decision_variable= self.decision_variable, investment_risk=self.investment_risk))
 
     def test_constraint_maximum_cost_for_low_risk_investments(self):
-        self.assertTrue(constraint_maximum_cost_for_low_risk_investments(self.projetos, self.decision_variable,self.custos, self.riscos))
+        self.assertTrue(constraint_maximum_cost_for_low_risk_investments(projects=self.projects,decision_variable= self.decision_variable,expected_cost_info=self.expected_cost, investment_risk=self.investment_risk,low_maximum_cost=1200000))
 
     def test_constraint_maximum_cost_for_medium_risk_investments(self):
-        self.assertTrue(constraint_maximum_cost_for_medium_risk_investments(self.projetos, self.decision_variable,self.custos, self.riscos))
+        self.assertTrue(constraint_maximum_cost_for_medium_risk_investments(projects=self.projects,decision_variable= self.decision_variable,expected_cost_info=self.expected_cost, investment_risk=self.investment_risk,average_maximum_cost=1500000))
 
     def test_constraint_maximum_cost_for_high_risk_investments(self):
-        self.assertTrue(constraint_maximum_cost_for_high_risk_investments(self.projetos, self.decision_variable,self.custos, self.riscos))
-    
+        self.assertTrue(constraint_maximum_cost_for_high_risk_investments(projects=self.projects,decision_variable= self.decision_variable,expected_cost_info=self.expected_cost, investment_risk=self.investment_risk,cost_maximum_high=900000))  
 
 
 
